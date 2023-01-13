21ad1710b5608dd0e51b9c2b4400d7a722ef7a96
Vivek Verma <vverma240420@gmail.com>
2023-01-13 19:34:46 +0530

    Add SwitchCase Concept

--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -6,24 +6,53 @@
         {
             Console.WriteLine("Welcome to Csharp basic programs");
 
-            Console.WriteLine("Please enter first number");
-            int first = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter second number");
-            int second = Convert.ToInt32(Console.ReadLine());
-
-            Calculator calculator1 = new Calculator(first, second);
-            calculator1.Addition();
-            Console.ReadLine();
+            Console.WriteLine("Please choose any one program from below options");
+            Console.WriteLine("1:Calculator\n2:SwitchCase\n10:Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
 
 
-            Console.WriteLine("Please enter third number");
-            int third = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter fourth number");
-            int fourth = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Please enter first number");
+                    int first = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter second number");
+                    int second = Convert.ToInt32(Console.ReadLine());
+
+                    Calculator calculator1 = new Calculator(first, second);
+                    calculator1.Addition();
+                    calculator1.Substraction();
+
+                    Console.WriteLine("Please enter third number");
+                    int third = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter fourth number");
+                    int fourth = Convert.ToInt32(Console.ReadLine());
 
 
-            Calculator calculator2 = new Calculator(third, fourth);
-            calculator2.Substraction();
+                    Calculator calculator2 = new Calculator(third, fourth);
+                    calculator2.Addition();
+                    calculator2.Substraction();
+
+                    break;
+
+                    case 2:
+
+                    Console.WriteLine("Please choose a day 'from 1 to 7");
+                    int day = Convert.ToInt32(Console.ReadLine());
+                    Weekday.FindADayWhichFallsOnWeek(day);
+
+                    break;
+
+                    case 10:
+                    break;
+
+                default:
+                    Console.WriteLine("Please choose number within given options");
+                    break;
+
+
+            }
+            
             Console.ReadLine();
         }
     }
---
+++ BasicProgram/Weekday.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class Weekday
+    {
+        public static void FindADayWhichFallsOnWeek(int dayNumber)
+        {
+            switch(dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("It's Sunday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("It's Monday");
+                    break;
+
+                  case 3:
+                    Console.WriteLine("It's Tuesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("It's Wedenesday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("It's Thursday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("It's Friday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("It's Saturday");
+                    break;
+
+                default:
+                    Console.WriteLine("Please choose a number between 1 to 7");
+                    break;
+            }
+        }
+    }
+}
